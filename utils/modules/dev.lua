91d73910620a8b2c1f259b436d46e66b4d4a13db
fxmmo <Jogoroblox86@gmail.com>
2026-07-05 17:01:20 -0300

    update utils/modules/dev.lua

---
+++ utils/modules/dev.lua
@@ -0,0 +1,24 @@
+local Dev = {}
+
+function Dev:New(file_configs)
+  local name = file_configs.Name 
+  local path = file_configs.Path 
+
+  local full_path = `{path}/{name}`
+  
+  if not (isfolder and isfile and makefolder and writefile) then
+    return
+  end
+
+  local save_file = full_path
+
+  if not isfolder(save_file) then
+    makefolder(path)
+
+    if not isfile(name) then
+      writefile(full_path, "{}")
+    end
+  end
+  
+  return full_path
+end
