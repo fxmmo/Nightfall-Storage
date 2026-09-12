8a1a4f71a9922d151e7d63d3f95fe5e61e9b64c1
fxmmo <Jogoroblox86@gmail.com>
2026-09-12 14:35:05 -0300

    Criar app.lua

---
+++ utils/modules/app.lua
@@ -0,0 +1,57 @@
+--!strict
+local app = {}
+
+local _cache = {}
+local _logs = {}
+
+local function log(...)
+  local msg = table.pack(...)
+  for i = 1, msg.n do 
+    table.insert(_logs, `[Debug]: {msg[i]}`)
+  end
+end
+
+function app:MakeDir(paths)
+  for _, path in paths do 
+    if isfolder and makefolder then 
+      if not isfolder(path) then
+        makefolder(path)
+      end 
+    end 
+  end 
+end
+
+function app:Import(src: string, path: string, name: string)
+  if not isfile and writefile then 
+    return false
+  end
+
+  local fullpath = `{path}/{name}`
+  if isfile(fullpath) then 
+    print("Retornando do arquivo")
+    return loadstring(readfile(fullpath))()
+  elseif _cache[src] then 
+    print("Retornando do cache")
+    return _cache[src]
+  end
+
+  local ok, res = pcall(function()
+      return game:HttpGet(src)
+    end)
+
+  if ok and res then 
+    local data = res 
+    _cache = data
+    
+    if path then 
+      self:MakeDir({path})
+      
+      if not isfile(fullpath) then 
+        writefile(fullpath, tostring(data))
+        print("Criando arquivo!")
+      end
+    end
+  else
+    log("Error while attempting to download", res)
+  end
+end
