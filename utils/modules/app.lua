1ba4dd9b9769a1d4f6be92ef897e25f32ea0ff06
fxmmo <Jogoroblox86@gmail.com>
2026-09-13 18:42:06 -0300

    Criar app.lua

---
+++ utils/modules/app.lua
@@ -0,0 +1,47 @@
+local service = {}
+service.__index = service
+
+service.__call = function(self, name: string)
+  return self.new(name)
+end
+  
+function service.new(name: string)
+  local self = setmetatable({}, service)
+  self.Name = name 
+  self.Folder = (makefolder and makefolder(name))
+  self.Children = {}
+
+  return self 
+end
+
+function service:Mkchild(name: string)
+  local fullpath = `{self.Folder}/{name}`
+  if isfolder and makefolder then 
+    if not isfolder(fullpath) then 
+      makefolder(fullpath)
+    end
+  end
+
+  local child = setmetatable({}, service)
+  child.Name = name 
+  child.Folder = fullpath
+  child.Children = {}
+
+  table.insert(self.Children, child)
+  return child
+end
+
+function service:Mkdir(paths)
+  for _, path in ipairs(paths) do
+    local fullpath = `{self.Folder}/{path}`
+    local child = string.split(fullpath, "/")
+    local part_path = ""
+      
+    for i, part in ipairs(child) do 
+        part_path = `{part_path}{(i > 1 and "/" or "")}{part}`
+        self:Mkchild(part_path)
+    end
+  end
+end
+  
+return service
